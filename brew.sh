3302fb95eb51ad57f6da26f900378a2c7644b155
vipic <read_xms@hotmail.com>
2024-02-18 15:31:33 +0800

    feat: make two scrips run only in arm64 architecture mac

--- brew.sh
+++ brew.sh
@@ -4,9 +4,18 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 
-# 将 brew 加入环境变量
-echo 'eval ,"$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# 将 brew 加入环境变量 (arm64架构的需要这个)
+arch=$(uname -m)
+
+if [ "$arch" = "arm64" ]; then
+    echo "执行arm架构的Mac上执行的操作"
+    echo 'eval ,"$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "不是arm架构的Mac，不执行任何操作"
+fi
+
+
 
 ##############################################
 ## 命令行应用
